4625aab3c0e72543f37615cf6b3b483f3acbab7e
menuin <menuin@hanyang.ac.kr>
2021-08-16 19:04:12 +0900

    #17.2 Select Photo part 1

--- navigators/UploadNav.js
+++ navigators/UploadNav.js
@@ -3,6 +3,7 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import SelectPhoto from "../screens/SelectPhoto";
 import TakePhoto from "../screens/TakePhoto";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createMaterialTopTabNavigator();
 const Stack = createStackNavigator();
@@ -22,8 +23,18 @@ export default function UploadNav() {
             }}>
             <Tab.Screen name="Select">
                 {() => (
-                    <Stack.Navigator>
-                        <Stack.Screen name="Select" component={SelectPhoto} />
+                    <Stack.Navigator screenOptions={{
+                        headerTintColor: "white",
+                        headerBackTitleVisible: false,
+                        headerBackImage: ({ tintColor }) => (
+                            <Ionicons color={tintColor} name="close" size={28} />
+                        ),
+                        headerStyle: {
+                            backgroundColor: "black",
+                            borderBottomColor: "#737373"
+                        }
+                    }}>
+                        <Stack.Screen name="Select" options={{ title: "Choose a photo" }} component={SelectPhoto} />
                     </Stack.Navigator>
                 )
                 }
