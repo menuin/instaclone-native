9264d0b4163570ff340665e31d20a96004175c2f
menuin <menuin@hanyang.ac.kr>
2021-08-17 03:33:51 +0900

    #17.4 Select Photo part 3

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import * as MediaLibrary from "expo-media-library";
+import { FlatList, Image, TouchableOpacity, useWindowDimensions } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 
 const Container = styled.View`
@@ -15,15 +17,24 @@ const Bottom = styled.View`
     flex : 1;
     background-color : black;
 `;
+const ImageContainer = styled.TouchableOpacity``
+const IconContainer = styled.TouchableOpacity`
+    position: absolute;
+    bottom : 5px;
+    right : 0px;
+`
 
 export default function SelectPhoto() {
+    const numColumns = 4;
+    const { width } = useWindowDimensions();
     const [ok, setOk] = useState(false);
     const [photos, setPhotos] = useState([]);
+    const [chosenPhoto, setChosenPhoto] = useState("");
+
     const getPhotos = async () => {
-        if (ok) {
-            const {assets:photos} = await MediaLibrary.getAssetsAsync();
-            setPhotos(photos)
-        }
+        const { assets: photos } = await MediaLibrary.getAssetsAsync();
+        setPhotos(photos)
+        setChosenPhoto(photos[0]?.uri);
     }
     const getPermissions = async () => {
         const { accessPrivileges, canAskAgain } = await MediaLibrary.getPermissionsAsync();
@@ -32,21 +43,49 @@ export default function SelectPhoto() {
 
             if (accessPrivileges !== "none") {
                 setOk(true);
+                getPhotos();
             }
         }
         else if (accessPrivileges !== "none") {
             setOk(true);
+            getPhotos();
         }
     }
     useEffect(() => {
         getPermissions();
-        getPhotos();
     }, [])
+
+    const choosePhoto = (uri) => {
+        setChosenPhoto(uri);
+    }
+    const renderItem = ({ item: photo }) => {
+        return (
+            <ImageContainer onPress={() => choosePhoto(photo.uri)}>
+                <Image source={{ uri: photo.uri }} style={{ width: width / numColumns, height: 100 }} />
+                <IconContainer>
+                    <Ionicons name="checkmark-circle" size={18} color="white" />
+                </IconContainer>
+            </ImageContainer>
+        )
+    }
+
     return (
         <Container>
-            <Top />
+            <Top>
+                {chosenPhoto !== "" ? (
+                    <Image
+                        source={{ uri: chosenPhoto }}
+                        style={{ width, height: "100%" }}
+                    />) : null}
+
+            </Top>
             <Bottom>
-                
+                <FlatList
+                    data={photos}
+                    numColumns={numColumns}
+                    keyExtractor={photo => photo.id}
+                    renderItem={renderItem}
+                />
             </Bottom>
         </Container>
     )
