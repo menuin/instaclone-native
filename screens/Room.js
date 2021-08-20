4098dc187d4bd9b91a4a19dc8a248d7153aa9812
menuin <menuin@hanyang.ac.kr>
2021-08-21 01:31:28 +0900

    #18.5 Room Screen part 3

--- screens/Room.js
+++ screens/Room.js
@@ -6,6 +6,15 @@ import ScreenLayout from "../components/ScreenLayout";
 import styled from "styled-components/native";
 import { FlatList, KeyboardAvoidingView, TextInput } from "react-native";
 
+const SEND_MESSAGE = gql`
+    mutation sendMessage($payload:String! $roomId:Int $userId:Int){
+        sendMessage(payload:$paylod, roomId:$roomId, userId:$userId){
+            ok
+            id
+        }
+    }
+`;
+
 const ROOM_QUERY = gql`
     query seeRoom($id:Int!){
         seeRoom(id:$id){
@@ -22,21 +31,35 @@ const ROOM_QUERY = gql`
     }
 `;
 
-const MessageContainer = styled.View``;
-const Author = styled.View``;
-const Avatar = styled.Image``;
-const Username = styled.Text`
-    color:white;
+const MessageContainer = styled.View`
+    padding : 10px 0px;
+    flex-direction: ${props => props.outgoing ? "row-reverse" : "row"};
+    align-items: flex-end;
+`;
+const Author = styled.View`
+    margin : 0px 10px;
+`;
+const Avatar = styled.Image`
+    height : 30px; 
+    width:30px;
+    border-radius:25px;
+    border-color:white;
 `;
 const Message = styled.Text`
     color:white;
+    background-color: rgba(255,255,255,0.4);
+    padding : 5px 10px;
+    border-radius : 10px;
+    overflow:hidden;
 `;
 const MessageInput = styled.TextInput`
-    margin-bottom:50px;
+    margin-bottom : 50px; 
+    margin-top:25px; 
     width:95%;
-    background-color: white;
+    border:1px solid rgba(255,255,255,0.5);
     padding : 10px 20px;
     border-radius:1000px;
+    font-size:16px;
 `;
 
 
@@ -54,10 +77,9 @@ export default function Room({ route, navigation }) {
     }, []);
 
     const renderItem = ({ item: message }) => (
-        <MessageContainer>
+        <MessageContainer outgoing={message.user.username !== route?.params?.talkingTo?.username}>
             <Author>
                 <Avatar source={{ uri: message.user.avatar }} />
-                <Username>{message.user.username}</Username>
             </Author>
             <Message>{message.payload}</Message>
         </MessageContainer>
@@ -65,7 +87,7 @@ export default function Room({ route, navigation }) {
 
 
     return (
-        <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "black" }} behavior="height" keyboardVerticalOffset={160}>
+        <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "black" }} behavior="padding" keyboardVerticalOffset={160}>
             <ScreenLayout loading={loading}>
                 <FlatList
                     inverted
@@ -75,6 +97,7 @@ export default function Room({ route, navigation }) {
                     renderItem={renderItem}
                 />
                 <MessageInput
+                    placeholderTextColor="rgba(255,255,255,0.5)"
                     placeholder="Write a message..."
                     returnKeyLabel="Send Message"
                     returnKeyType="send"
